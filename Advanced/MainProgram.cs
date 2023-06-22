70867575266d83f64bb122c21787d661a3a00cd6
BlueBear93 <ljh93420@gmail.com>
2023-06-23 00:59:48 +0900

    Day04

--- Advanced/MainProgram.cs
+++ Advanced/MainProgram.cs
@@ -10,19 +10,56 @@ namespace Advanced
     {
         static void Main(string[] args)
         {
-            Console.Write("숫자입력:");
-            // int age = int.Parse(Console.ReadLine());
+            string pwd = "wasd";
 
-            int age;
-            bool bFlag = int.TryParse(Console.ReadLine(), out age);
-            if (bFlag)
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine(age);
+                Console.Write("Enter an access code: ");
+                if (pwd == Console.ReadLine())
+                {
+                    Console.WriteLine("Access granted. Starting the program...");
+                    MainSelections(); // 진짜 프로그램 실행
+                    return; // 프로그램 종료
+                }
+                else
+                {
+                    Console.WriteLine("Invalid code. Try again.\n");
+                }
             }
-            else
+
+            Console.WriteLine("Access denied. Too many incorrect attempts. Exiting the program...");
+        }
+
+        static void MainSelections()
+        {
+            Console.WriteLine("Welcome to the Azurk Console Prototype.");
+            Console.WriteLine("Select one of the following options:");
+            Console.WriteLine("1. Projects\n2. GPT Samples\n3. Quit Console");
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
+
+            switch (keyInfo.Key)
             {
-                Console.WriteLine("숫자를 입력하세요.");
+                case ConsoleKey.D1:
+                    Console.WriteLine("You selected 'Projects'.");
+                    break;
+                case ConsoleKey.D2:
+                    Console.WriteLine("You selected 'GPT Samples'.");
+                    break;
+                case ConsoleKey.D3:
+                    Console.WriteLine("Quitting the console.");
+                    Quit();
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection. Please choose 1, 2, or 3.");
+                    break;
             }
+
+        }
+
+        static void Quit()
+        {
+            return;
         }
     }
 }
