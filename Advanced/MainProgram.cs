42f0fd477b11edc13dc943aa0214944a392adaf7
AzureBear <ljh93420@gmail.com>
2023-06-22 17:51:28 +0900

    Day04

--- Advanced/MainProgram.cs
+++ Advanced/MainProgram.cs
@@ -8,5 +8,21 @@ namespace Advanced
 {
     class MainProgram
     {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자입력:");
+            // int age = int.Parse(Console.ReadLine());
+
+            int age;
+            bool bFlag = int.TryParse(Console.ReadLine(), out age);
+            if (bFlag)
+            {
+                Console.WriteLine(age);
+            }
+            else
+            {
+                Console.WriteLine("숫자를 입력하세요.");
+            }
+        }
     }
 }
